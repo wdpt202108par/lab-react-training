e49f7c1bfa3fe455384cafe67985a9c0598741f7
Lea Beaulieu <beaulieu.lea@free.fr>
2022-01-11 19:53:33 +0100

    iteration 1 without css

---
+++ src/idcard/IdCard.js
@@ -0,0 +1,31 @@
+import React, { Component} from 'react';
+import './IdCard.css';
+
+
+/* Iteration 1 | Component: IdCard */
+
+class IdCard extends Component {
+/* if no component ligne 1, "extends React.Component" */ 
+    
+  render (){
+    return (
+      <div className="Container">
+
+        <img src={this.props.picture} alt="picture" />
+        
+        <div>
+          <ul>
+            <li><strong>Last Name : </strong>{this.props.lastName}</li>
+            <li><strong>First Name : </strong>{this.props.firstName}</li>
+            <li><strong>Gender : </strong>{this.props.gender}</li>
+            <li><strong>Height : </strong>{this.props.height/100} m</li>
+            <li><strong>Birth : </strong>{this.props.birth.formatDate}</li>
+          </ul>
+        </div>
+      </div>  
+    )
+  }
+    
+}
+
+export default IdCard;
